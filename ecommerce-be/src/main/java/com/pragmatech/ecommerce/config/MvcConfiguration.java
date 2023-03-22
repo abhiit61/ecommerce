7450692ef99b2406daad269a0c504375be846daf
Abhishek Upadhyay <abhiit61@gmail.com>
2023-03-22 12:42:30 +0530

    added allowed hosts

--- ecommerce-be/src/main/java/com/pragmatech/ecommerce/config/MvcConfiguration.java
+++ ecommerce-be/src/main/java/com/pragmatech/ecommerce/config/MvcConfiguration.java
@@ -10,7 +10,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://192.168.1.17:3000","http://122.170.109.102:3000")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .exposedHeaders("page-total-count")
                 .exposedHeaders("page-total-elements")
